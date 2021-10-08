d0e7c312c27d7bf02b443288f3086e7c869c2e65
pup-paw <wldusdl0310@gmail.com>
2021-10-08 21:59:04 +0900

    2021-10-08(1)

--- silver5/2941.py
+++ silver5/2941.py
@@ -1,11 +1,5 @@
 l = input()
 c = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-# n = 0
-# for i in range(len(c)):
 for x in c:
-    # if l.count(c[i]) != 0:
-    #     n += l.count(c[i])
     l = l.replace(x, ',')
-# l = l.replace(',', '')
-# print(n + len(l))
 print(len(l))
