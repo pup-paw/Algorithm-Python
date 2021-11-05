3c4c0ae3abb533c1d0daea934d23e1e45a2da84e
pup-paw <wldusdl0310@gmail.com>
2021-11-05 15:32:15 +0900

    2021-11-05

--- Programmers/level1/failure_rate.py
+++ Programmers/level1/failure_rate.py
@@ -1,9 +1,14 @@
 def solution(N, stages):
     faillure = {i: 0 for i in range(1, N+1)}
-    for stage in stages:
-        if stage > 1:
-            faillure[stage-1] += 1
-    return sorted(faillure.items(), key=lambda x: x[1])
+    stage = len(stages)
+    for i in faillure.keys():
+        stage -= stages.count(i-1)
+        if stage == 0:
+            break
+        faillure[i] = stages.count(i) / stage
+    # return list(
+    #     dict(sorted(faillure.items(), key=lambda x: x[1], reverse=True)).keys())
+    return sorted(faillure, key=lambda x: faillure[x], reverse=True)
 
 
 print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
